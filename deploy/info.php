29ba7ef7cd66f5891c493c7a118c6a30602c0a0e
pcbaldwin <pbaldwin@clearfoundation.com>
2015-03-27 14:08:45 -0400

    Prepped for build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'pptpd';
-$app['version'] = '2.0.18';
+$app['version'] = '2.0.21';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-pptpd.spec
+++ packaging/app-pptpd.spec
@@ -1,7 +1,7 @@
 
 Name: app-pptpd
 Epoch: 1
-Version: 2.0.18
+Version: 2.0.21
 Release: 1%{dist}
 Summary: PPTP Server
 License: GPLv3
