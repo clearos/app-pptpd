23a0dc2a457a0309c3030d2cdc792dadd0875d60
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:30:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN వినియోగదారులకు సాధారణ చాలా ఆపరేటింగ్ వ్యవస్థలు ఒక VPN క్లయింట్ ఉపయోగించి మీ నెట్వర్క్కు కనెక్ట్ అనుమతిస్తుంది. PPTP సులభం మరియు రోడ్డు యోధులు ఉపయోగకరంగా ఉంది కానీ OpenVPN వంటి ఇతర సాంకేతికతలు కంటే తక్కువ సురక్షితం భావిస్తారు.';
+$lang['pptpd_app_name'] = 'PPTP సర్వర్';
+$lang['pptpd_auto_configure_help'] = 'PPTP సర్వర్ ప్రస్తుతం ఆటో ఆకృతీకరణ రీతిలో ఉంది. మీరు మీ నెట్వర్కు అమరికలను మారిస్తే, PPTP సర్వర్ తెలివిగా దాని సెట్టింగ్లను అవసరమైన మార్పులు చేస్తుంది. మీరు ఆటో ఆకృతీకరణ డిసేబుల్ చెయ్యాలనుకుంటే, క్రింది బటన్ పై క్లిక్ చేయండి.';
+$lang['pptpd_dns_server'] = 'DNS సర్వర్';
+$lang['pptpd_dns_server_invalid'] = 'DNS సర్వర్ చెల్లదు.';
+$lang['pptpd_ip_range_invalid'] = 'ఐపీ శ్రేణి చెల్లదు.';
+$lang['pptpd_local_ip_range'] = 'స్థానిక IP రేంజ్';
+$lang['pptpd_pptp_server'] = 'PPTP సర్వర్';
+$lang['pptpd_remote_ip_range'] = 'రిమోట్ ఐపీ రేంజ్';
+$lang['pptpd_wins_server'] = 'WINS సర్వర్';
+$lang['pptpd_wins_server_invalid'] = 'WINS సర్వర్ చెల్లదు.';
