f7f8e2700c2f2e0c068334102bf458e02d114d61
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:29:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN lumella basebelisi ho hokahanya le ho hokahanngoa ea hao sebedisa VPN sedirelwa tloaelehile ho litsamaiso fetisisa ho sebelisa mehaho. PPTP bonolo le le thuso ya ho bahlabani tseleng empa ho nkoa se sireletsehileng ka tlase theknoloji e &#39;ngoe e kang OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'The PPTP seva ke hona joale screen reader mode likoloi-phetolo. Haeba u fetoha di-setting ya netweke oa hao, PPTP seva tla bohlale etsa liphetoho tse hlokahalang ho di-setting tsa eona. Haeba u ka rata ho go tima likoloi-phetolo, ka kōpo tobetsa konopo e latelang.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS seva ha o sebetse.';
+$lang['pptpd_ip_range_invalid'] = 'IP tsena lia fetana ha o sebetse.';
+$lang['pptpd_local_ip_range'] = 'Local IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Ka thōko IP Range';
+$lang['pptpd_wins_server'] = 'hapa Server';
+$lang['pptpd_wins_server_invalid'] = 'Hapa seva ha o sebetse.';
