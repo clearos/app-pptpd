7f876ce8c59bdbc784ca19c58666e6bdd852cf1d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:32:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN gba awọn olumulo lati sopọ si nẹtiwọki rẹ nipa lilo a VPN ni ose wọpọ si julọ awọn ọna šiše. PPTP jẹ rorun ati ki o wulo fun opopona alagbara sugbon ti wa ni ka kere ni aabo ju miiran imo bi OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'Awọn PPTP server wa ni Lọwọlọwọ ni idojukọ-iṣeto ni mode. Ti o ba ti o ba yi nẹtiwọki rẹ eto, awọn PPTP server yoo intelligently ṣe awọn ayipada si awọn oniwe-eto. Ti o ba yoo fẹ lati mu idojukọ-iṣeto ni, jọwọ tẹ lori awọn wọnyi bọtini.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS server ni invalid.';
+$lang['pptpd_ip_range_invalid'] = 'IP ibiti o ti wa ni invalid.';
+$lang['pptpd_local_ip_range'] = 'Agbegbe IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Latọna IP Range';
+$lang['pptpd_wins_server'] = 'AamiEye Server';
+$lang['pptpd_wins_server_invalid'] = 'AamiEye olupin ni invalid.';
