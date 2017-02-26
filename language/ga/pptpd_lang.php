d9ae4b80f45bbab601d95fc57d51e3a0da03906f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:24:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN chumas úsáideoirí chun ceangal le do líonra ag baint úsáide as cliant VPN a mbaineann formhór na córais oibriúcháin. Is PPTP éasca agus úsáideach do laochra bóthair ach meastar chomh daingean ná teicneolaíochtaí eile ar nós OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Freastalaí';
+$lang['pptpd_auto_configure_help'] = 'Is é an freastalaí PPTP i mód uathoibríoch-cumraíochta. Má athraíonn tú do chuid socruithe líonra, beidh an fhreastalaí PPTP dhéanamh intelligently na hathruithe is gá ar a suímh. Más mian leat a chur ar ceal auto-chumraíocht, cliceáil ar an gcnaipe seo a leanas.';
+$lang['pptpd_dns_server'] = 'Freastalaí DNS';
+$lang['pptpd_dns_server_invalid'] = 'Is freastalaí DNS neamhbhailí.';
+$lang['pptpd_ip_range_invalid'] = 'Is raon IP neamhbhailí.';
+$lang['pptpd_local_ip_range'] = 'Local Raon IP';
+$lang['pptpd_pptp_server'] = 'PPTP Freastalaí';
+$lang['pptpd_remote_ip_range'] = 'Iargúlta IP Range';
+$lang['pptpd_wins_server'] = 'WINS Freastalaí';
+$lang['pptpd_wins_server_invalid'] = 'Is WINS freastalaí neamhbhailí.';
