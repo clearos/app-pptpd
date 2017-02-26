b6e0de97e0c75dea8b2adb2a38ee8dfe935f4dc9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:21:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN erabiltzaile zure sarean VPN bezero bat erabiliz konektatzeko sistema eragile gehienen ahalbidetzen. PPTP erraza eta erabilgarria errepide gudarien da baina jotzen da beste teknologia OpenVPN atsegin bezain segurua.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'Une PPTP zerbitzaria da auto-konfigurazioa moduan. sareko ezarpenak aldatzen baduzu, PPTP zerbitzariak adimentsuan egiteko bere ezarpenak behar diren aldaketak. auto-konfigurazioa desgaitzeko nahi izanez gero, mesedez, klikatu hurrengo botoian.';
+$lang['pptpd_dns_server'] = 'DNS zerbitzaria';
+$lang['pptpd_dns_server_invalid'] = 'DNS zerbitzaria baliogabea da.';
+$lang['pptpd_ip_range_invalid'] = 'IP barrutia ez da zuzena.';
+$lang['pptpd_local_ip_range'] = 'Bertako IP barrutia';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Urrutiko IP barrutia';
+$lang['pptpd_wins_server'] = 'WINS zerbitzari';
+$lang['pptpd_wins_server_invalid'] = 'WINS zerbitzari baliogabea da.';
