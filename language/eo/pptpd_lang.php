7b4292a218772da5e6fe48d0e396c64d460b01ad
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:23:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN permesas uzantojn konekti al via reto uzante VPN kliento komuna al plej operaciumoj. PPTP estas facila kaj utila por vojo soldatoj sed estas konsiderita malpli sekura ol aliaj teknologioj kiel OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Servilo';
+$lang['pptpd_auto_configure_help'] = 'La PPTP servilo estas aktuale en aŭto- agordo modo. Se vi ŝanĝas vian reton agordojn, la PPTP servilo inteligente fari la necesajn ŝanĝojn al lia agordojn. Se vi ŝatus malŝalti aŭtomatan agordon, bonvolu klaki la sekvan butonon.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS servilo estas nevalida.';
+$lang['pptpd_ip_range_invalid'] = 'IP gamo estas nevalida.';
+$lang['pptpd_local_ip_range'] = 'Lokaj IP Gamo';
+$lang['pptpd_pptp_server'] = 'PPTP Servilo';
+$lang['pptpd_remote_ip_range'] = 'Fora IP Gamo';
+$lang['pptpd_wins_server'] = 'WINS Servilo';
+$lang['pptpd_wins_server_invalid'] = 'WINS servilo estas nevalida.';
