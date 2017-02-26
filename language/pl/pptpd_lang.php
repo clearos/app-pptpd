ac2d1a2bd4350c12a74e2bc440a89e3f4a06f7a8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:28:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN pozwala użytkownikom na łączenie się z siecią przy użyciu klienta VPN wspólne dla większości systemów operacyjnych. PPTP jest łatwe i przydatne dla użytkowników mobilnych, ale jest uważane za mniej bezpieczne niż inne technologie, takie jak OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'Serwer PPTP jest aktualnie w trybie auto-konfiguracji. Jeśli zmienisz ustawienia sieciowe, serwer PPTP inteligentnie dokonać koniecznych zmian w swoich ustawieniach. Jeśli chcesz wyłączyć automatyczną konfigurację, kliknij na poniższy przycisk.';
+$lang['pptpd_dns_server'] = 'Serwer DNS';
+$lang['pptpd_dns_server_invalid'] = 'Serwer DNS jest nieprawidłowy.';
+$lang['pptpd_ip_range_invalid'] = 'Zakres adresów IP jest nieprawidłowy.';
+$lang['pptpd_local_ip_range'] = 'Lokalny Zakres IP';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Zakres IP zdalnego';
+$lang['pptpd_wins_server'] = 'Serwer WINS';
+$lang['pptpd_wins_server_invalid'] = 'Serwer WINS jest nieprawidłowy.';
