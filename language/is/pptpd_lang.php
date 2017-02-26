52e9d4c6419035ed889ff43d7f82dda96e626274
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:25:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN gerir notendum kleift að tengja til þinn net með því að nota VPN viðskiptavinur algengt að flest stýrikerfi. PPTP er auðvelt og gagnlegt fyrir stríðsmenn vegum en er talin ótryggari en önnur tækni eins OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'The PPTP framreiðslumaður er nú í sjálfvirka stillingaskránni ham. Ef þú breytir netstillingum er PPTP framreiðslumaður vilja greindur gera nauðsynlegar breytingar á stillingum þess. Ef þú vildi eins og til að slökkva á sjálfvirkri stillingu skaltu smella á eftirfarandi hnapp.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS framreiðslumaður er ógilt.';
+$lang['pptpd_ip_range_invalid'] = 'IP svið er ógilt.';
+$lang['pptpd_local_ip_range'] = 'Local IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Remote IP Range';
+$lang['pptpd_wins_server'] = 'WINS miðlara';
+$lang['pptpd_wins_server_invalid'] = 'WINS miðlara er ógilt.';
