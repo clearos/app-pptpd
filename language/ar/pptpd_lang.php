150d22326ae3b2c4d41909031a0730a35d985982
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:21:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN يسمح للمستخدمين الاتصال بالشبكة الخاصة بك باستخدام عميل VPN الشائعة في معظم أنظمة التشغيل. PPTP هو سهل ومفيد للمحاربين الطريق ولكنه يعتبر أقل أمنا من غيرها من التقنيات مثل المسنجر.';
+$lang['pptpd_app_name'] = 'PPTP خادم';
+$lang['pptpd_auto_configure_help'] = 'خادم PPTP هو حاليا في وضع التكوين التلقائي. إذا قمت بتغيير إعدادات الشبكة، سيقوم الملقم PPTP جعل بذكاء التغييرات اللازمة لإعداداته. إذا كنت ترغب في تعطيل التكوين التلقائي، يرجى الضغط على زر التالي.';
+$lang['pptpd_dns_server'] = 'خادم DNS';
+$lang['pptpd_dns_server_invalid'] = 'ملقم DNS غير صالح.';
+$lang['pptpd_ip_range_invalid'] = 'مجموعة IP غير صالح.';
+$lang['pptpd_local_ip_range'] = 'نطاق IP المحلي';
+$lang['pptpd_pptp_server'] = 'PPTP خادم';
+$lang['pptpd_remote_ip_range'] = 'المدى البعيد IP';
+$lang['pptpd_wins_server'] = 'ملقم WINS';
+$lang['pptpd_wins_server_invalid'] = 'ملقم WINS غير صالح.';
