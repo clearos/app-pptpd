edca0f0db463c08b081991c6248182730557222c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:27:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN वापरकर्ते सामान्य कार्य प्रणाली एक व्हीपीएन क्लाएंट वापरून आपल्या नेटवर्कशी कनेक्ट करण्याची परवानगी देते. PPTP रस्ता वॉरियर्स सोपे आणि उपयुक्त आहे पण OpenVPN सारख्या इतर तंत्रज्ञान कमी सुरक्षित मानली जाते.';
+$lang['pptpd_app_name'] = 'PPTP सर्व्हर';
+$lang['pptpd_auto_configure_help'] = 'PPTP सर्व्हर सध्या स्वयं-संरचना मोड मध्ये आहे. आपण आपल्या नेटवर्क सेटिंग्ज बदलू, तर PPTP सर्व्हर बौद्धिक, त्याची सेटिंग्ज आवश्यक ते बदल केले जातील. आपण स्वयं-संरचना अकार्यक्षम इच्छित असल्यास, खालील बटणावर क्लिक करा.';
+$lang['pptpd_dns_server'] = 'DNS सर्व्हर';
+$lang['pptpd_dns_server_invalid'] = 'DNS सर्व्हर अवैध आहे.';
+$lang['pptpd_ip_range_invalid'] = 'आंतरजाल श्रेणी अवैध आहे.';
+$lang['pptpd_local_ip_range'] = 'स्थानिक आंतरजाल श्रेणी';
+$lang['pptpd_pptp_server'] = 'PPTP सर्व्हर';
+$lang['pptpd_remote_ip_range'] = 'रिमोट IP श्रेणी';
+$lang['pptpd_wins_server'] = 'WINS सर्व्हर';
+$lang['pptpd_wins_server_invalid'] = 'WINS सर्व्हर अवैध आहे.';
