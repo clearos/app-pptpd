66e6e68664d3e5718e427a040317621358f539fb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:28:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN permite utilizatorilor să se conecteze la rețea folosind un client VPN comun pentru majoritatea sistemelor de operare. PPTP este ușor și util pentru războinici rutier, dar este considerată mai puțin sigură decât alte tehnologii, cum ar fi OpenVPN.';
+$lang['pptpd_app_name'] = 'Serverul PPTP';
+$lang['pptpd_auto_configure_help'] = 'Server PPTP este în prezent în modul auto-configurare. Dacă modificați setările de rețea, serverul PPTP va face în mod inteligent modificările necesare la setările sale. În cazul în care doriți să dezactivați auto-configurare, vă rugăm să faceți clic pe butonul de mai jos.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'server DNS este nevalid.';
+$lang['pptpd_ip_range_invalid'] = 'Gama de IP este nevalid.';
+$lang['pptpd_local_ip_range'] = 'Gama de IP locală';
+$lang['pptpd_pptp_server'] = 'Serverul PPTP';
+$lang['pptpd_remote_ip_range'] = 'IP la distanță Interval';
+$lang['pptpd_wins_server'] = 'Server WINS';
+$lang['pptpd_wins_server_invalid'] = 'WINS server este nevalid.';
