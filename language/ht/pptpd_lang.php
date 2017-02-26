5ac9794c1b3667074d9a8f25c8a5315f4bb6d5ce
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:24:04 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'Ptp vpn pèmèt itilizatè yo konekte nan rezo w lan lè l sèvi avèk yon kliyan vpn komen nan pi sistèm operasyon. Ptp se fasil epi itil pou vanyan sòlda wout men se konsidere kòm mwens sekirite pase lòt teknoloji tankou OpenVPN.';
+$lang['pptpd_app_name'] = 'ptp sèvè';
+$lang['pptpd_auto_configure_help'] = 'Sèvè a ptp se kounye a nan mòd oto-konfigirasyon. Si ou chanje anviwònman rezo w lan, sèvè a ptp pral intelijans fè chanjman ki nesesè nan anviwònman li yo. Si ou ta renmen vin enfim oto-konfigirasyon, tanpri klike sou bouton ki anba la a.';
+$lang['pptpd_dns_server'] = 'dns sèvè';
+$lang['pptpd_dns_server_invalid'] = 'sèvè dns se valab.';
+$lang['pptpd_ip_range_invalid'] = 'seri IP se valab.';
+$lang['pptpd_local_ip_range'] = 'Lokal IP Range';
+$lang['pptpd_pptp_server'] = 'ptp sèvè';
+$lang['pptpd_remote_ip_range'] = 'Remote IP Range';
+$lang['pptpd_wins_server'] = 'WINS sèvè';
+$lang['pptpd_wins_server_invalid'] = 'WINS sèvè se valab.';
