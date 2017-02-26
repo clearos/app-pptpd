f00b95b69d460e78b1923e82ccfeb83990c3e58a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:26:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN leidžia vartotojams prisijungti prie tinklo, naudojant VPN klientas bendri daugumai operacinių sistemų. PPTP yra lengva ir naudinga keliaujantiems, tačiau manoma mažiau saugus nei kitų technologijų, pavyzdžiui, OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Serveris';
+$lang['pptpd_auto_configure_help'] = 'PPTP serveris šiuo metu yra auto-konfigūracijos režimas. Jei pakeisite savo tinklo nustatymus, PPTP serveris bus protingai padaryti būtinus pakeitimus į savo nustatymus. Jei norite išjungti automatinį konfigūravimą, spustelėkite šį mygtuką.';
+$lang['pptpd_dns_server'] = 'DNS serveris';
+$lang['pptpd_dns_server_invalid'] = 'DNS serveris yra neteisinga.';
+$lang['pptpd_ip_range_invalid'] = 'IP diapazonas yra neteisingas.';
+$lang['pptpd_local_ip_range'] = 'Vietinis IP adresų';
+$lang['pptpd_pptp_server'] = 'PPTP Serveris';
+$lang['pptpd_remote_ip_range'] = 'Nuotolinio IP klasės';
+$lang['pptpd_wins_server'] = 'WINS serveris';
+$lang['pptpd_wins_server_invalid'] = 'WINS serverio negalioja.';
