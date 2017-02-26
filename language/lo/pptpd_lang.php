a606aaffee33375bdebf40313c69f3c96d4592b3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:26:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN ອະນຸຍາດໃຫ້ຜູ້ໃຊ້ໃນການເຊື່ອມຕໍ່ກັບເຄືອຂ່າຍຂອງທ່ານໂດຍນໍາໃຊ້ລູກຄ້າ VPN ທົ່ວໄປລະບົບປະຕິບັດການຫຼາຍທີ່ສຸດ. PPTP ເປັນເລື່ອງງ່າຍແລະເປັນປະໂຫຍດສໍາລັບ warriors ຖະຫນົນຫົນທາງແຕ່ພິຈາລະນາຄວາມປອດໄພຫນ້ອຍກ່ວາເຕັກໂນໂລຊີອື່ນທີ່ຄ້າຍຄື OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'ເຄື່ອງແມ່ຂ່າຍຂອງ PPTP ປະຈຸບັນແມ່ນຢູ່ໃນຮູບແບບອັດຕະໂນມັດການຕັ້ງຄ່າ. ຖ້າຫາກວ່າທ່ານມີການປ່ຽນແປງການຕັ້ງຄ່າເຄືອຂ່າຍຂອງທ່ານ, ເຄື່ອງແມ່ຂ່າຍຂອງ PPTP ສະຕິປັນຍາທີ່ຈະເຮັດໃຫ້ການປ່ຽນແປງທີ່ຈໍາເປັນເພື່ອປັບຄ່າຂອງຕົນ. ຖ້າຫາກວ່າທ່ານຕ້ອງການທີ່ຈະປິດການທໍາງານອັດຕະໂນມັດການຕັ້ງຄ່າ, ກະລຸນາຄລິກໃສ່ປຸ່ມດັ່ງຕໍ່ໄປນີ້.';
+$lang['pptpd_dns_server'] = 'Server DNS';
+$lang['pptpd_dns_server_invalid'] = 'ເຄື່ອງແມ່ຂ່າຍຂອງ DNS ບໍ່ຖືກຕ້ອງ.';
+$lang['pptpd_ip_range_invalid'] = 'ລະດັບ IP ບໍ່ຖືກຕ້ອງ.';
+$lang['pptpd_local_ip_range'] = 'Range IP ທ້ອງຖິ່ນ';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Range IP ຫ່າງໄກສອກຫຼີກ';
+$lang['pptpd_wins_server'] = 'WINS Server';
+$lang['pptpd_wins_server_invalid'] = 'ເຄື່ອງແມ່ຂ່າຍຂອງ WINS ແມ່ນບໍ່ຖືກຕ້ອງ.';
