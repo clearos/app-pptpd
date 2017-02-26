69ca7fa1fe521e6f065c1e52a5f9c515f4e97fdc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:23:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN võimaldab kasutajal ühendada oma võrku kasutades VPN-kliendi, mis enamike operatsioonisüsteemidega. PPTP on lihtne ja kasulik kaugemal käinud, kuid peetakse vähem turvaline kui teised tehnoloogiad, nagu OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'PPTP server on praegu auto-konfiguratsiooni mode. Kui muudad oma võrgu seadeid, PPTP server arukalt teha vajalikud muudatused oma seadeid. Kui soovid blokeerida automaatse konfiguratsiooni, siis klõpsa järgmisele nuppu.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS server ei sobi.';
+$lang['pptpd_ip_range_invalid'] = 'IP vahemik on vigane.';
+$lang['pptpd_local_ip_range'] = 'Kohalik IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'IP- Range';
+$lang['pptpd_wins_server'] = 'WINS serveri';
+$lang['pptpd_wins_server_invalid'] = 'WINS server ei sobi.';
