d6676bc6e8955ea12dc665727e3d993d527a5d79
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:23:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN avulla käyttäjät voivat muodostaa yhteyden verkkoon VPN yhteisiä useimmille käyttöjärjestelmille. PPTP on helppoa ja hyödyllistä tie sotureita, mutta sitä pidetään vähemmän turvallinen kuin muut tekniikat, kuten OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'PPTP-palvelin on tällä hetkellä auto-asetustilan. Jos muutat verkkoasetuksia, PPTP-palvelin älykkäästi tehdä tarvittavat muutokset sen asetuksiin. Jos haluat poistaa automaattisen kokoonpanon, klikkaa seuraavaa painiketta.';
+$lang['pptpd_dns_server'] = 'DNS-palvelin';
+$lang['pptpd_dns_server_invalid'] = 'DNS-palvelin on virheellinen.';
+$lang['pptpd_ip_range_invalid'] = 'IP-alue ei kelpaa.';
+$lang['pptpd_local_ip_range'] = 'Paikallinen IP-alue';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Kauko IP-alue';
+$lang['pptpd_wins_server'] = 'WINS-palvelin';
+$lang['pptpd_wins_server_invalid'] = 'WINS-palvelin on virheellinen.';
