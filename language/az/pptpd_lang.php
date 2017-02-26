e00b4764afede8f1a3267f0bd4852a280ccc7b20
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:21:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN users ümumi ən əməliyyat sistemi üçün bir VPN müştəri istifadə edərək, şəbəkə qoşulmaq imkan verir. PPTP yol döyüşçülər üçün asan və faydalı, lakin OpenVPN kimi digər texnologiyaları az təhlükəsiz hesab olunur.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'PPTP server avtomatik konfiqurasiya rejimi hazırda. Siz ağ ayarlarını dəyişdirmək, PPTP server ağıllı onun parametrləri lazımi dəyişikliklər edəcək. avtomatik konfiqurasiya aradan istəyirsinizsə, aşağıdakı &#39;düyməsinə basın.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS server yanlışdır.';
+$lang['pptpd_ip_range_invalid'] = 'IP range yanlışdır.';
+$lang['pptpd_local_ip_range'] = 'Yerli IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Remote IP Range';
+$lang['pptpd_wins_server'] = 'WINS Server';
+$lang['pptpd_wins_server_invalid'] = 'WINS server yanlışdır.';
