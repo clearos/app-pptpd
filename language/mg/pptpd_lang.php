6d3f60573812345600cd4b8aaac69815a14e8ba6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:26:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN mamela ireo mpampiasa azy ny tambajotra mifandray amin&#39;ny fampiasana ny VPN mpanjifa mahazatra ny ankamaroan&#39;ny rafitra fandidiana. Mora PPTP sy ny mahasoa ho an&#39;ny lalana miaramila, fa tsy heverina ho voaro noho ny teknolojia hafa toy ny OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'Ny PPTP mpizara amin&#39;izao fotoana izao ao amin&#39;ny Auto-fanahafana fomba. Raha hanova ny tambajotra toe-javatra, ny PPTP mpizara dia tamim-pahendrena hanao ny fanovana ilaina ho amin&#39;ny toe-javatra. Raha te-mankarary Auto-fanahafana, azafady tsindrio eo amin&#39;ny bokotra manaraka.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS mpizara dia tsy manan-kery.';
+$lang['pptpd_ip_range_invalid'] = 'IP isan-karazany dia tsy manan-kery.';
+$lang['pptpd_local_ip_range'] = 'Local IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Remote IP Range';
+$lang['pptpd_wins_server'] = 'Fandresena Server';
+$lang['pptpd_wins_server_invalid'] = 'Fandresena mpizara dia tsy manan-kery.';
