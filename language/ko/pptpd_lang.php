13c79b6f05313ccdeadc076779ef7aaf29606eb2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:25:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN은 사용자가 일반적인 대부분의 운영 체제에 VPN 클라이언트를 사용하여 네트워크에 연결할 수 있습니다. PPTP는 도로 전사에 대한 쉽고 유용하지만 OpenVPN을 같은 다른 기술보다 보안 수준이 낮은 것으로 간주됩니다.';
+$lang['pptpd_app_name'] = 'PPTP 서버';
+$lang['pptpd_auto_configure_help'] = 'PPTP 서버 자동 설정 모드에서 현재. 네트워크 설정을 변경하면 PPTP 서버는 지능적으로 해당 설정에 필요한 사항을 변경합니다. 당신이 자동 구성을 해제하려는 경우, 다음 버튼을 클릭하시기 바랍니다.';
+$lang['pptpd_dns_server'] = 'DNS 서버';
+$lang['pptpd_dns_server_invalid'] = 'DNS 서버가 잘못되었습니다.';
+$lang['pptpd_ip_range_invalid'] = 'IP 범위가 잘못되었습니다.';
+$lang['pptpd_local_ip_range'] = '로컬 IP 범위';
+$lang['pptpd_pptp_server'] = 'PPTP 서버';
+$lang['pptpd_remote_ip_range'] = '원격 IP 범위';
+$lang['pptpd_wins_server'] = 'WINS 서버';
+$lang['pptpd_wins_server_invalid'] = 'WINS 서버가 잘못되었습니다.';
