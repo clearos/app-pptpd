8aea8431f2ac6f373de119c827b58eec64c0306d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:28:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN ਉਪਭੋਗੀ ਸਭ ਓਪਰੇਟਿੰਗ ਸਿਸਟਮ ਨੂੰ ਆਮ ਇੱਕ VPN ਕਲਾਇਟ ਵਰਤ ਨੂੰ ਆਪਣੇ ਨੈੱਟਵਰਕ ਨਾਲ ਕੁਨੈਕਟ ਕਰਨ ਲਈ ਸਹਾਇਕ ਹੈ. PPTP ਆਸਾਨ ਅਤੇ ਸੜਕ ਵਾਰੀਅਰਜ਼ ਲਈ ਲਾਭਦਾਇਕ ਹੈ, ਪਰ ਦਿੱਤਾ ਵਰਗੇ ਹੋਰ ਤਕਨਾਲੋਜੀ ਵੱਧ ਘੱਟ ਸੁਰੱਖਿਅਤ ਮੰਨਿਆ ਗਿਆ ਹੈ.';
+$lang['pptpd_app_name'] = 'PPTP ਸਰਵਰ';
+$lang['pptpd_auto_configure_help'] = 'PPTP ਸਰਵਰ ਆਟੋ-ਸੰਰਚਨਾ ਢੰਗ ਵਿੱਚ ਇਸ ਵੇਲੇ ਹੈ. ਤੁਹਾਨੂੰ ਆਪਣੇ ਨੈੱਟਵਰਕ ਸੈਟਿੰਗ ਨੂੰ ਤਬਦੀਲ ਹੋ, PPTP ਸਰਵਰ ਸਿਆਣਪ ਇਸ ਦੀ ਸੈਟਿੰਗ ਕਰਨ ਲਈ ਜ਼ਰੂਰੀ ਬਦਲਾਅ ਕਰਨ ਜਾਵੇਗਾ. ਤੁਹਾਨੂੰ ਆਟੋ-ਸੰਰਚਨਾ ਨੂੰ ਆਯੋਗ ਕਰਨ ਲਈ ਚਾਹੁੰਦੇ ਹੋ, ਹੇਠ ਬਟਨ ਤੇ ਕਲਿੱਕ ਕਰੋ.';
+$lang['pptpd_dns_server'] = 'DNS ਸਰਵਰ';
+$lang['pptpd_dns_server_invalid'] = 'DNS ਸਰਵਰ ਗਲਤ ਹੈ.';
+$lang['pptpd_ip_range_invalid'] = 'IP ਸੀਮਾ ਗਲਤ ਹੈ.';
+$lang['pptpd_local_ip_range'] = 'ਸਥਾਨਕ IP ਸੀਮਾ';
+$lang['pptpd_pptp_server'] = 'PPTP ਸਰਵਰ';
+$lang['pptpd_remote_ip_range'] = 'ਰਿਮੋਟ IP ਸੀਮਾ';
+$lang['pptpd_wins_server'] = 'WINS ਸਰਵਰ';
+$lang['pptpd_wins_server_invalid'] = 'WINS ਸਰਵਰ ਗਲਤ ਹੈ.';
