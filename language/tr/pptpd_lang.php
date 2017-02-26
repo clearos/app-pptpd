9de47fed19b982f63fc12d665eb3c114ca3edf9d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:31:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN kullanıcıları ortak birçok işletim sistemi için bir VPN istemcisi kullanarak ağınıza bağlanmasına izin verir. PPTP yol savaşçıları için kolay ve kullanışlı ama OpenVPN gibi diğer teknolojilere göre daha az güvenli olarak kabul edilir.';
+$lang['pptpd_app_name'] = 'PPTP Sunucusu';
+$lang['pptpd_auto_configure_help'] = 'PPTP sunucu otomatik yapılandırma modunda şu anda. ağ ayarlarını değiştirirseniz, PPTP sunucusu akıllıca onun ayarlarında gerekli değişiklikleri yapacaktır. Otomatik yapılandırmayı devre dışı bırakmak istiyorsanız, aşağıdaki butona tıklayınız.';
+$lang['pptpd_dns_server'] = 'DNS Sunucusu';
+$lang['pptpd_dns_server_invalid'] = 'DNS sunucusu geçersiz.';
+$lang['pptpd_ip_range_invalid'] = 'IP aralığı geçersiz.';
+$lang['pptpd_local_ip_range'] = 'Yerel IP Aralığı';
+$lang['pptpd_pptp_server'] = 'PPTP Sunucusu';
+$lang['pptpd_remote_ip_range'] = 'Uzak IP Aralığı';
+$lang['pptpd_wins_server'] = 'WINS Sunucusu';
+$lang['pptpd_wins_server_invalid'] = 'WINS sunucusu geçersiz.';
