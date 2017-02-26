fc3c25fc9f3a48ab8ca89df8efe6eef998760dcf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:31:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN cho phép người dùng kết nối với mạng của bạn sử dụng một máy khách VPN phổ biến với hầu hết các hệ điều hành. PPTP là dễ dàng và hữu ích cho các chiến binh đường nhưng được xem là kém an toàn hơn so với các công nghệ khác như OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP server';
+$lang['pptpd_auto_configure_help'] = 'Các máy chủ PPTP hiện ở chế độ tự động cấu hình. Nếu bạn thay đổi các thiết lập mạng, máy chủ PPTP sẽ thông minh thực hiện các thay đổi cần thiết để cài đặt. Nếu bạn muốn vô hiệu hóa tính năng tự động cấu hình, bạn hãy click vào nút sau.';
+$lang['pptpd_dns_server'] = 'DNS server';
+$lang['pptpd_dns_server_invalid'] = 'máy chủ DNS là không hợp lệ.';
+$lang['pptpd_ip_range_invalid'] = 'Dải IP không hợp lệ.';
+$lang['pptpd_local_ip_range'] = 'IP động địa phương';
+$lang['pptpd_pptp_server'] = 'PPTP server';
+$lang['pptpd_remote_ip_range'] = 'Từ xa IP động';
+$lang['pptpd_wins_server'] = 'WINS server';
+$lang['pptpd_wins_server_invalid'] = 'WINS server là không hợp lệ.';
