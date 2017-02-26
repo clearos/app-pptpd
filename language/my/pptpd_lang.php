d482aa2ea3ee5a4da256e161c8242adf913b21a0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:22:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN ကိုသုံးစွဲသူများကအများဆုံးလည်ပတ်မှုစနစ်များအားဘုံတစ် VPN ကို client ကိုအသုံးပြုပြီးသင့် network ကိုချိတ်ဆက်ရန်ခွင့်ပြုသည်။ PPTP လမ်းသူရဲများအတွက်လွယ်ကူပြီးအသုံးဝင်သည်ဒါပေမဲ့ OpenVPN ကိုတူအခြားနည်းပညာများထက်လျော့နည်းလုံခြုံစဉ်းစားသည်။';
+$lang['pptpd_app_name'] = 'PPTP ကို ​​Server ကို';
+$lang['pptpd_auto_configure_help'] = 'အဆိုပါ PPTP server ကို Auto-configuration ကို mode မှာလက်ရှိဖြစ်ပါတယ်။ သင်သည်သင်၏ကွန်ယက်ကို setting များကိုပြောင်းလဲပစ်လျှင်, PPTP server ကိုလိမ်မာပါးနပ်သည်၎င်း၏ setting များကိုရန်လိုအပ်သောအပြောင်းအလဲများဖြစ်စေမည်။ သငျသညျ Auto-configuration ကို disable လုပ်ဖို့လိုပါလျှင်အောက်ပါ button ကို click ပါ။';
+$lang['pptpd_dns_server'] = 'DNS ကို Server ကို';
+$lang['pptpd_dns_server_invalid'] = 'DNS Server ကိုမမှန်ကန်ဘူး။';
+$lang['pptpd_ip_range_invalid'] = 'အိုင်ပီအကွာအဝေးမမှန်ကန်ဘူး။';
+$lang['pptpd_local_ip_range'] = 'ဒေသခံအိုင်ပီ Range';
+$lang['pptpd_pptp_server'] = 'PPTP ကို ​​Server ကို';
+$lang['pptpd_remote_ip_range'] = 'ဝေးလံခေါင်သီသော IP Range';
+$lang['pptpd_wins_server'] = 'ကိုအနိုင်ပေး Server ကို';
+$lang['pptpd_wins_server_invalid'] = 'ကိုအနိုင်ပေး server ကိုမမှန်ကန်ဘူး။';
