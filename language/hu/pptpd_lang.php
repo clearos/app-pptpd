5d5614af6d1262c81d70c0ad50cadb82a2d19eec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:24:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN lehetővé teszi a felhasználók csatlakozni a hálózatra VPN kliens a legtöbb operációs rendszer. PPTP egyszerű és hasznos, akik sokat utaznak, de kevésbé tartják biztonságosabb, mint más technológiák, mint OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP szerver';
+$lang['pptpd_auto_configure_help'] = 'A PPTP szerver jelenleg automatikus konfigurációs üzemmódban. Ha megváltoztatja a hálózati beállításokat, a PPTP szerver intelligensen elvégzi a szükséges változtatásokat annak beállításait. Ha le szeretné tiltani az automatikus konfiguráció, kérjük kattintson az alábbi gombra.';
+$lang['pptpd_dns_server'] = 'DNS-kiszolgáló';
+$lang['pptpd_dns_server_invalid'] = 'DNS-kiszolgáló érvénytelen.';
+$lang['pptpd_ip_range_invalid'] = 'IP tartomány érvénytelen.';
+$lang['pptpd_local_ip_range'] = 'Helyi IP-tartomány';
+$lang['pptpd_pptp_server'] = 'PPTP szerver';
+$lang['pptpd_remote_ip_range'] = 'Távoli IP tartomány';
+$lang['pptpd_wins_server'] = 'WINS szerver';
+$lang['pptpd_wins_server_invalid'] = 'WINS szerver érvénytelen.';
