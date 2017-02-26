d0bcd03576d56a51cf0ea737d3f8af51d69c835c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:26:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN permittit users uti VPN client annecto ut vestri network operating ratio communis omnium. PPTP est utile, et est via viri, sed minus tuto quam aliae Technologies sicut OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'PPTP server est currently in in configuration auto-modum. Si mutare Network occasus, ut intelligenter PPTP Mutatis mutandis ad occasus erit servo. Si vis inactivandi auto- figurationem eius respondeant, placere click in sequenti felis.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'Irrita DNS moderatruo.';
+$lang['pptpd_ip_range_invalid'] = 'IP range est invalidum.';
+$lang['pptpd_local_ip_range'] = 'Locus IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Remote IP Range';
+$lang['pptpd_wins_server'] = 'wins Server';
+$lang['pptpd_wins_server_invalid'] = 'Wins server est invalidum.';
