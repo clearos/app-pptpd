da9da0f6a9795b57e4a321300ea69fc7a971a331
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:30:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN inaruhusu watumiaji kuungana na mtandao wako kwa kutumia VPN mteja kawaida kwa mifumo ya uendeshaji. PPTP ni rahisi na muhimu kwa ajili ya wapiganaji barabara lakini ni kuchukuliwa salama chini ya teknolojia nyingine kama OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'PPTP server ipo katika modi ya auto-Configuration. Kama mabadiliko ya mazingira ya mtandao wako, PPTP server itakuwa akili kufanya mabadiliko ya lazima kwa mipangilio yake. Kama ungependa afya auto-Configuration, tafadhali bonyeza kifungo zifuatazo.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS server ni batili.';
+$lang['pptpd_ip_range_invalid'] = 'IP mbalimbali ni batili.';
+$lang['pptpd_local_ip_range'] = 'Mitaa IP Mbalimbali';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Kijijini IP Mbalimbali';
+$lang['pptpd_wins_server'] = 'WINS Server';
+$lang['pptpd_wins_server_invalid'] = 'WINS server ni batili.';
