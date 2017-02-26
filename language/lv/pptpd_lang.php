66fc667f2a8b05b82e294415f7c8b56b6ad9dd4a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:26:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN ļauj lietotājiem izveidot savienojumu ar tīklu, izmantojot VPN klients kopējs vairumam operētājsistēmām. PPTP ir vienkārša un noderīga ceļu karavīri, bet tiek uzskatīta par mazāk droša nekā citām tehnoloģijām, piemēram, OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'PPTP serveri šobrīd ir auto-konfigurācijas režīmā. Ja jūs mainīt tīkla iestatījumus, PPTP serveri būs gudri veikt nepieciešamās izmaiņas tās iestatījumus. Ja vēlaties atslēgt auto-konfigurāciju, lūdzu, noklikšķiniet uz šīs pogas.';
+$lang['pptpd_dns_server'] = 'DNS serveris';
+$lang['pptpd_dns_server_invalid'] = 'DNS serveris ir nederīgs.';
+$lang['pptpd_ip_range_invalid'] = 'IP diapazons ir nederīgs.';
+$lang['pptpd_local_ip_range'] = 'Local IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Remote IP Range';
+$lang['pptpd_wins_server'] = 'WINS serveris';
+$lang['pptpd_wins_server_invalid'] = 'WINS serveris ir nederīgs.';
