102f6e855e7a5edf0f35d68300bbcb508520cbec
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:27:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN प्रयोगकर्ता साधारण भन्दा सञ्चालन प्रणाली गर्न एउटा VPN ग्राहक प्रयोग गरेर आफ्नो नेटवर्क जडान गर्न अनुमति दिन्छ। पिपिटिपि सडक योद्धाहरू लागि सजिलो र उपयोगी छ तर OpenVPN जस्तै अरू प्रविधिहरू भन्दा कम सुरक्षित मानिन्छ।';
+$lang['pptpd_app_name'] = 'पिपिटिपि सर्भर';
+$lang['pptpd_auto_configure_help'] = 'यो पिपिटिपि सर्भर स्वत: कन्फिगरेसन मोडमा छ। तपाईं आफ्नो नेटवर्क सेटिङहरू परिवर्तन भने, पिपिटिपि सर्भर बौद्धिक यसको सेटिङ गर्न आवश्यक परिवर्तन हुनेछ। तपाईं स्वतः कन्फिगरेसन असक्षम गर्न चाहनुहुन्छ भने, निम्न बटन मा क्लिक गर्नुहोस्।';
+$lang['pptpd_dns_server'] = 'DNS सर्भर';
+$lang['pptpd_dns_server_invalid'] = 'DNS सर्भर अमान्य छ।';
+$lang['pptpd_ip_range_invalid'] = 'आईपी ​​दायरा अवैध छ।';
+$lang['pptpd_local_ip_range'] = 'स्थानीय आईपी सीमा';
+$lang['pptpd_pptp_server'] = 'पिपिटिपि सर्भर';
+$lang['pptpd_remote_ip_range'] = 'रिमोट आईपी सीमा';
+$lang['pptpd_wins_server'] = 'WINS सर्भर';
+$lang['pptpd_wins_server_invalid'] = 'WINS सर्भर अमान्य छ।';
