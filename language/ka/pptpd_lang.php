f8a69892fdc83a1f65549d532a1837cfcc127ae1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:25:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN საშუალებას აძლევს მომხმარებლებს, რათა დაუკავშირდეთ თქვენს ქსელის გამოყენებით VPN კლიენტის საერთო საუკეთესო ოპერაციული სისტემები. PPTP არის მარტივი და სასარგებლო საგზაო მეომრები მაგრამ ითვლება ნაკლებად უსაფრთხო, ვიდრე სხვა ტექნოლოგიები, როგორიცაა OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'PPTP სერვერზე ამჟამად ავტომატური კონფიგურაცია რეჟიმში. თუ თქვენ შეცვლის თქვენი ქსელის პარამეტრები, PPTP სერვერზე ჭკვიანურად მიიღოს საჭირო ცვლილებები მისი პარამეტრები. თუ გსურთ გამორთოთ ავტომატური კონფიგურაცია, გთხოვთ დააჭირეთ შემდეგ ღილაკს.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS სერვერზე არასწორია.';
+$lang['pptpd_ip_range_invalid'] = 'IP დიაპაზონი არასწორია.';
+$lang['pptpd_local_ip_range'] = 'Local IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'დისტანციური IP Range';
+$lang['pptpd_wins_server'] = 'WINS სერვერი';
+$lang['pptpd_wins_server_invalid'] = 'WINS სერვერზე არასწორია.';
