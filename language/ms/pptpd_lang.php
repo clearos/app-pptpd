0a59718258cb00b54c8e0daf04c5aaf7f040b8b6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:27:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN membolehkan pengguna untuk menyambung ke rangkaian anda menggunakan klien VPN biasa untuk sistem operasi yang paling. PPTP mudah dan berguna untuk pahlawan-pahlawan jalan tetapi dianggap kurang selamat daripada teknologi lain seperti OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'Pelayan PPTP kini dalam mod auto-konfigurasi. Jika anda menukar tetapan rangkaian anda, pelayan PPTP yang bijak akan membuat perubahan yang perlu untuk tetapannya. Jika anda ingin untuk melumpuhkan auto-konfigurasi, sila klik pada butang berikut.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'server DNS tidak sah.';
+$lang['pptpd_ip_range_invalid'] = 'pelbagai IP tidak sah.';
+$lang['pptpd_local_ip_range'] = 'Julat IP tempatan';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Remote Julat IP';
+$lang['pptpd_wins_server'] = 'WINS Server';
+$lang['pptpd_wins_server_invalid'] = 'pelayan WINS tidak sah.';
