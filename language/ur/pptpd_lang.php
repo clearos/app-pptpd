031d231c5847311b049999916b780f37cc86154a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:31:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN صارفین عام سب سے زیادہ آپریٹنگ سسٹمز کے لئے ایک VPN کلائنٹ کا استعمال کرتے ہوئے آپ کے نیٹ ورک سے رابطہ قائم کرنے کی اجازت دیتا ہے. PPTP سڑک یودقاوں کے لئے آسان اور مفید ہے لیکن اوپن طرح دیگر ٹیکنالوجیز کے مقابلے میں کم محفوظ تصور کیا جاتا ہے.';
+$lang['pptpd_app_name'] = 'PPTP سرور';
+$lang['pptpd_auto_configure_help'] = 'PPTP سرور آٹو ترتیب موڈ میں فی الحال ہے. آپ کو آپ کے نیٹ ورک کی ترتیبات کو تبدیل کرتے ہیں، PPTP سرور ہوشیاری اس کی ترتیبات کے لئے ضروری تبدیلیاں کریں گے. آپ آٹو کنفیگریشن کو غیر فعال کرنا چاہتے ہیں تو، درج ذیل بٹن پر کلک کریں براہ مہربانی.';
+$lang['pptpd_dns_server'] = 'DNS سرور';
+$lang['pptpd_dns_server_invalid'] = 'DNS سرور غلط ہے.';
+$lang['pptpd_ip_range_invalid'] = 'IP رینج غلط ہے.';
+$lang['pptpd_local_ip_range'] = 'لوکل IP رینج';
+$lang['pptpd_pptp_server'] = 'PPTP سرور';
+$lang['pptpd_remote_ip_range'] = 'ریموٹ IP رینج';
+$lang['pptpd_wins_server'] = 'WINS سرور';
+$lang['pptpd_wins_server_invalid'] = 'WINS سرور غلط ہے.';
