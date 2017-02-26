9da21c8818ce49de26a8c766e5dab95bdc30e9db
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:30:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN ба корбарон имкон медиҳад пайваст ба шабакаи худро бо истифода аз як муштарӣ VPN умумӣ ба ҳама системаҳои оператсионӣ. PPTP осон ва муфид аст, барои ҷанговарони роҳ мебошад, вале ҳисоб эмин камтар аз технологияҳои дигар ба монанди OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'Сервер PPTP аст, айни замон дар ҳолати худкор-конфигуратсия ғайрифаъол месозад. Агар шумо танзимоти шабақаи шумо иваз, сервери PPTP ба intelligently хоҳад тағйироти зарурӣ ба танзимоти он кунад. Агар шумо мехоҳед ба хомӯш худкор-танзимотро таъғир кунед, лутфан ба тугмаи зерин пахш кунед.';
+$lang['pptpd_dns_server'] = 'DNS сервер';
+$lang['pptpd_dns_server_invalid'] = 'сервер DNS нодуруст аст.';
+$lang['pptpd_ip_range_invalid'] = 'доираи IP нодуруст аст.';
+$lang['pptpd_local_ip_range'] = 'Маҳаллӣ IP Диапазони';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Дурдасти IP Диапазони';
+$lang['pptpd_wins_server'] = 'пирӯзиҳои Server';
+$lang['pptpd_wins_server_invalid'] = 'Пирӯзиҳои сервер нодуруст аст.';
