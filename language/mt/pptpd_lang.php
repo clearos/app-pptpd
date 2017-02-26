cb36d7582598a649557a179a4dd10db8ceadf7c4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:27:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN tippermetti lill-utenti għall-konnessjoni ma netwerk tiegħek tuża klijent VPN komuni għas-sistemi aktar operattivi. PPTP huwa faċli u utli għall-ġellieda triq iżda hija kkunsidrata inqas sikuri minn teknoloġiji oħra bħall-OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'Il-server PPTP huwa attwalment fil-modalità auto-konfigurazzjoni. Jekk tibdel settings network tiegħek, il-server PPTP intelliġenti se tagħmel il-bidliet meħtieġa settings tiegħu. Jekk inti tixtieq li jiskonnettja l-awto-konfigurazzjoni, jekk jogħġbok ikklikkja fuq il-buttuna ta &#39;wara.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'server DNS huwa invalidu.';
+$lang['pptpd_ip_range_invalid'] = 'firxa IP huwa invalidu.';
+$lang['pptpd_local_ip_range'] = 'Medda PI Lokali';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Remote Medda PI';
+$lang['pptpd_wins_server'] = 'WINS Server';
+$lang['pptpd_wins_server_invalid'] = 'WINS server huwa invalidu.';
