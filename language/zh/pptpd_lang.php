44a7a8ea5ba576df824c89a6d761e8b32c3e7f01
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:22:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN允许用户连接到使用VPN客户端网络中常见的大多数操作系统。 PPTP是很容易的和有用的道路战士，但被认为比其他技术，如OpenVPN的安全性较低。';
+$lang['pptpd_app_name'] = 'PPTP服务器';
+$lang['pptpd_auto_configure_help'] = '目前，PPTP服务器处于自动配置模式。 如果您更改网络设置，PPTP服务器将智能地做出它的设置进行必要的修改。 如果您想禁用自动配置，请点击以下按钮。';
+$lang['pptpd_dns_server'] = 'DNS服务器';
+$lang['pptpd_dns_server_invalid'] = 'DNS服务器是无效的。';
+$lang['pptpd_ip_range_invalid'] = 'IP范围无效。';
+$lang['pptpd_local_ip_range'] = '本地IP范围';
+$lang['pptpd_pptp_server'] = 'PPTP服务器';
+$lang['pptpd_remote_ip_range'] = '远程IP范围';
+$lang['pptpd_wins_server'] = 'WINS服务器';
+$lang['pptpd_wins_server_invalid'] = 'WINS服务器是无效的。';
