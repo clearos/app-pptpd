716129b614f77535b209f6032df688bbd010d33d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:29:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN බොහෝ භාවිතා කරන්නන් මෙහෙයුම් පද්ධති සඳහා පොදු වූ අතාත්වික පෞද්ගලික ජාලය සේවාදායකයා භාවිතා කර ඔබගේ ජාලය සමඟ සම්බන්ධ වීම සඳහා ඉඩ සලසයි. PPTP මාර්ග රණශූර පහසු හා ප්රයෝජනවත් වන නමුත් OpenVPN වැනි වෙනත් තාක්ෂණයන් වඩා අඩු සුරක්ෂිත ලෙස සැලකේ.';
+$lang['pptpd_app_name'] = 'PPTP සේවාදායකය';
+$lang['pptpd_auto_configure_help'] = 'මෙම PPTP සේවාදායකය ස්වයංක්රීය-වින්යාසය මාදිලිය දැනට. ඔබ ඔබේ ජාල සැකසීම් වෙනස් නම්, PPTP සේවාදායකය බුද්ධිමත්ව එහි සැකසුම් සඳහා අවශ්ය වෙනස්කම් කරනු ඇත. ඔබ ස්වයංක්රීය-වින්යාසය අක්රිය කිරීමට කැමති නම්, පහත දැක්වෙන බොත්තම මත ක්ලික් කරන්න.';
+$lang['pptpd_dns_server'] = 'DNS සේවාදායක';
+$lang['pptpd_dns_server_invalid'] = 'DNS සේවාදායකයේ වලංගු නැත.';
+$lang['pptpd_ip_range_invalid'] = 'IP පරාසය අවලංගුය.';
+$lang['pptpd_local_ip_range'] = 'කියන IP රංගේ';
+$lang['pptpd_pptp_server'] = 'PPTP සේවාදායකය';
+$lang['pptpd_remote_ip_range'] = 'දුරස්ථ IP රංගේ';
+$lang['pptpd_wins_server'] = 'ජය සේවාදායකය';
+$lang['pptpd_wins_server_invalid'] = 'ජය සේවාදායකය වලංගු නැත.';
