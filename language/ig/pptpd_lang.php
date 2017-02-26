07b5646ce4571b5268e9f56bb95157c95d4cd609
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:24:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP Okwey ohere ọrụ ijikọ na netwọk gị site na iji a Okwey ahịa nkịtị ka ọtụtụ sistem. PPTP dị mfe na bara uru n&#39;ihi na okporo ụzọ dike ma a na-ewere obere ala karịa ndị ọzọ na teknụzụ dị ka OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'The PPTP server bụ ugbu a na akpaaka-nhazi mode. Ọ bụrụ na ị gbanwee netwọk gị ntọala, PPTP nkesa ga amamihe ime mgbanwe ndị na-ya ntọala. Ọ bụrụ na ị ga-achọ iji gbanyụọ auto-nhazi, biko pịa ndị na-esonụ button.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS nkesa bụ ghara ịdị irè.';
+$lang['pptpd_ip_range_invalid'] = 'IP nso bụ ghara ịdị irè.';
+$lang['pptpd_local_ip_range'] = 'Obodo IP iche';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'N&#39;ime obodo IP iche';
+$lang['pptpd_wins_server'] = '-enweta nkwanye Server';
+$lang['pptpd_wins_server_invalid'] = '-Enweta nkwanye server bụ ghara ịdị irè.';
