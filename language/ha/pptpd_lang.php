de4c8aa31e8956e7f4d6f3313a2bef377119b41a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:24:12 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN damar masu amfani to connect to your cibiyar sadarwa ta amfani da VPN abokin ciniki kowa ga mafi aiki tsarin. PPTP ne mai sauki da kuma amfani ga hanya warriors amma an dauki kasa amintacce fiye da sauran kimiyoyi kamar OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'The PPTP uwar garken ne a halin yanzu a auto-sanyi mode. Idan ka canza hanyar sadarwa saituna, PPTP uwar garke zai intelligently yi zama dole canje-canje zuwa da saituna. Idan ka so ka musaki auto-sanyi, don Allah danna kan wadannan button.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS uwar garken ba daidai ba ne.';
+$lang['pptpd_ip_range_invalid'] = 'IP range ba daidai ba ne.';
+$lang['pptpd_local_ip_range'] = 'Local IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Nesa IP Range';
+$lang['pptpd_wins_server'] = 'wins Server';
+$lang['pptpd_wins_server_invalid'] = 'Wins uwar garken ba daidai ba ne.';
