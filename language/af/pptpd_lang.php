9c43ef9e61c17389f7a292ff994438feb6b3507f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:20:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP Skynprivaatnetwerk gebruikers toelaat om toegang tot die netwerk met behulp van &#39;n Skynprivaatnetwerk kliënt gemeen die meeste bedryfstelsels. PPTP is maklik en nuttig vir die pad krygers, maar word beskou as minder veilig as ander tegnologie soos OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP-bediener';
+$lang['pptpd_auto_configure_help'] = 'Die PPTP-bediener is tans in die motor-opset af. As jy jou netwerk instellings te verander, sal die PPTP-bediener intelligent maak die nodige veranderinge aan sy instellings. As jy wil graag outomatiese verstellings te skakel, kliek op die volgende knoppie.';
+$lang['pptpd_dns_server'] = 'DNS-bediener';
+$lang['pptpd_dns_server_invalid'] = 'DNS-bediener is ongeldig.';
+$lang['pptpd_ip_range_invalid'] = 'IP reeks is ongeldig.';
+$lang['pptpd_local_ip_range'] = 'Plaaslike IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP-bediener';
+$lang['pptpd_remote_ip_range'] = 'Remote IP Range';
+$lang['pptpd_wins_server'] = 'WINS Server';
+$lang['pptpd_wins_server_invalid'] = 'WINS-bediener is ongeldig.';
