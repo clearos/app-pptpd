20dabc5b31ece0ff61fe4b16b23ebb68b9d44cf1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:22:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hr/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN omogućuje korisnicima spajanje na mrežu pomoću VPN klijent zajedničke za većinu operativnih sustava. PPTP je jednostavan i koristan za ceste ratnika, ali se smatra manje siguran od drugih tehnologija kao što su OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP poslužitelj';
+$lang['pptpd_auto_configure_help'] = 'PPTP poslužitelj je trenutno u auto-konfiguracijski način rada. Ako promijenite postavke mreže, PPTP poslužitelj će inteligentno napraviti potrebne promjene u svojim postavkama. Ako želite onemogućiti automatsko konfiguraciju, molimo kliknite na sljedeći gumb.';
+$lang['pptpd_dns_server'] = 'DNS poslužitelj';
+$lang['pptpd_dns_server_invalid'] = 'DNS poslužitelj nije valjan.';
+$lang['pptpd_ip_range_invalid'] = 'IP raspon je nevažeći.';
+$lang['pptpd_local_ip_range'] = 'Lokalna IP raspona';
+$lang['pptpd_pptp_server'] = 'PPTP poslužitelj';
+$lang['pptpd_remote_ip_range'] = 'Daljinski IP raspona';
+$lang['pptpd_wins_server'] = 'WINS poslužitelj';
+$lang['pptpd_wins_server_invalid'] = 'WINS poslužitelja nije valjan.';
