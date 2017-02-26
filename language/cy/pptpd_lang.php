28f03e9bcd7005072c3d0819e355823f2ef0b898
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:31:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN caniatáu defnyddwyr i gysylltu â&#39;ch rhwydwaith gan ddefnyddio cleient VPN gyffredin i&#39;r rhan fwyaf o systemau gweithredu. PPTP yn hawdd ac yn ddefnyddiol ar gyfer rhyfelwyr ffordd ond yn cael ei ystyried yn llai diogel na thechnolegau eraill fel OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Gweinydd';
+$lang['pptpd_auto_configure_help'] = 'Mae&#39;r gweinydd PPTP ar hyn o bryd yn y modd auto-ffurfweddu. Os byddwch yn newid eich gosodiadau rhwydwaith, bydd y gweinydd PPTP ddeallus gwneud y newidiadau angenrheidiol i&#39;w lleoliadau. Os hoffech chi analluogi auto-ffurfweddu, cliciwch ar y botwm isod.';
+$lang['pptpd_dns_server'] = 'Gweinydd DNS';
+$lang['pptpd_dns_server_invalid'] = 'gweinydd DNS yn annilys.';
+$lang['pptpd_ip_range_invalid'] = 'amrediad IP yn annilys.';
+$lang['pptpd_local_ip_range'] = 'Lleol Ystod IP';
+$lang['pptpd_pptp_server'] = 'PPTP Gweinydd';
+$lang['pptpd_remote_ip_range'] = 'Pell IP Range';
+$lang['pptpd_wins_server'] = 'ENNILL Gweinydd';
+$lang['pptpd_wins_server_invalid'] = 'ENNILL gweinydd yn annilys.';
