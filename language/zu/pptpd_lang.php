e67a794a8887702b0a114109db5c728be6513baf
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:32:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN ivumela abasebenzisi ukuxhuma kunethiwekhi yakho usebenzisa iklayenti ye-VPN ezivame ukuba izinhlelo kakhulu yokusebenza. PPTP kulula futhi ewusizo for amabutho emgwaqeni kodwa ubhekwa ivikeleke kancane kuka nobunye ubuchwepheshe like OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'Iseva ye-PPTP okwamanje auto-ukumisa imodi. Uma sishintshe okulungiselelwe kohleloxhumano zakho, iseva PPTP ngeke ngokukhalipha wenze izinguquko ezidingekayo ukuze kuzilungiselelo zalo. Uma ungathanda ukuba ukukhubaza auto-ukumisa, sicela uchofoze inkinobho elandelayo.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS iseva ayivumelekile.';
+$lang['pptpd_ip_range_invalid'] = 'IP uhla ayivumelekile.';
+$lang['pptpd_local_ip_range'] = 'Local IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Remote IP Range';
+$lang['pptpd_wins_server'] = 'WINS Server';
+$lang['pptpd_wins_server_invalid'] = 'WINS iseva ayivumelekile.';
