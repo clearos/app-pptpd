f4b000d2108cc755b83fd07c274788007536aa8d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:24:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hi/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP वीपीएन उन सबसे ऑपरेटिंग सिस्टम के लिए आम एक वीपीएन ग्राहक का उपयोग कर अपने नेटवर्क से कनेक्ट करने की अनुमति देता है। PPTP आसान और सड़क योद्धाओं के लिए उपयोगी है, लेकिन OpenVPN जैसे अन्य तकनीकों की तुलना में कम सुरक्षित माना जाता है।';
+$lang['pptpd_app_name'] = 'PPTP सर्वर';
+$lang['pptpd_auto_configure_help'] = 'PPTP सर्वर ऑटो-विन्यास मोड में है। आप अपने नेटवर्क सेटिंग्स बदलते हैं, PPTP सर्वर समझदारी से अपनी सेटिंग्स के लिए आवश्यक परिवर्तन कर देगा। आप ऑटो-विन्यास को निष्क्रिय करना चाहते हैं, तो निम्न बटन पर क्लिक करें।';
+$lang['pptpd_dns_server'] = 'डीएनएस सर्वर';
+$lang['pptpd_dns_server_invalid'] = 'डीएनएस सर्वर अमान्य है।';
+$lang['pptpd_ip_range_invalid'] = 'आईपी ​​रेंज अमान्य है।';
+$lang['pptpd_local_ip_range'] = 'स्थानीय आईपी रेंज';
+$lang['pptpd_pptp_server'] = 'PPTP सर्वर';
+$lang['pptpd_remote_ip_range'] = 'रिमोट आईपी रेंज';
+$lang['pptpd_wins_server'] = 'WINS सर्वर';
+$lang['pptpd_wins_server_invalid'] = 'WINS सर्वर अमान्य है।';
