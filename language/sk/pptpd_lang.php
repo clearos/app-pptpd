f5026d81c28ada7e7fabdf965e0f1016239b6fd5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:29:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sk/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN umožňuje užívateľom pripojiť k sieti pomocou VPN klienta spoločné pre väčšinu operačných systémov. PPTP je jednoduché a užitočné pre cestách, ale je považovaná za menej bezpečná ako iné technológie, ako je OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'PPTP server je v súčasnosti v režime automatickej konfigurácie. Ak zmeníte nastavenie siete, bude PPTP servera inteligentne vykonať potrebné zmeny v jej nastavení. Ak by ste chceli zakázať automatickú konfiguráciu, kliknite na nasledujúce tlačidlo.';
+$lang['pptpd_dns_server'] = 'DNS server';
+$lang['pptpd_dns_server_invalid'] = 'DNS server je neplatný.';
+$lang['pptpd_ip_range_invalid'] = 'IP rozsah je neplatná.';
+$lang['pptpd_local_ip_range'] = 'Miestny rozsah IP';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Vzdialený rozsah IP';
+$lang['pptpd_wins_server'] = 'WINS server';
+$lang['pptpd_wins_server_invalid'] = 'WINS server je neplatný.';
