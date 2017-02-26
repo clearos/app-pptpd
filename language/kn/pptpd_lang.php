4a17de0840ab82c3b5a01e3d04dfe656c5f0c15f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:25:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN ಬಳಕೆದಾರರಿಗೆ ಸಾಮಾನ್ಯ ಹೆಚ್ಚಿನ ಕಾರ್ಯಾಚರಣಾ ವ್ಯವಸ್ಥೆಗಳು ಒಂದು VPN ಕ್ಲೈಂಟ್ ಬಳಸಿ ನಿಮ್ಮ ನೆಟ್ವರ್ಕ್ಗೆ ಸಂಪರ್ಕಿಸಲು ಅನುಮತಿಸುತ್ತದೆ. PPTP ರಸ್ತೆ ಯೋಧರು ಸುಲಭ ಮತ್ತು ಉಪಯುಕ್ತ ಆದರೆ VPN ತೆರೆಯಿರಿ ತಂತ್ರಜ್ಞಾನಕ್ಕೆ ಕಡಿಮೆ ಸುರಕ್ಷಿತ ಪರಿಗಣಿಸಲಾಗುತ್ತದೆ.';
+$lang['pptpd_app_name'] = 'PPTP ಸರ್ವರ್';
+$lang['pptpd_auto_configure_help'] = 'PPTP ಸರ್ವರ್ಗೆ ಪ್ರಸ್ತುತ ಸ್ವಯಂ ಸಂರಚನಾ ಕ್ರಮದಲ್ಲಿ ಹೊಂದಿದೆ. ನಿಮ್ಮ ಜಾಲಬಂಧ ಸಿದ್ಧತೆಗಳನ್ನು ಬದಲಾಯಿಸಿದರೆ, PPTP ಸರ್ವರ್ ಬುದ್ಧಿವಂತಿಕೆಯಿಂದ ಅದರ ಸೆಟ್ಟಿಂಗ್ಗಳನ್ನು ಅಗತ್ಯ ಬದಲಾವಣೆಗಳನ್ನು ಮಾಡಲು. ನೀವು ಸ್ವಯಂ ಸಂರಚನಾ ನಿಷ್ಕ್ರಿಯಗೊಳಿಸಲು ಬಯಸಿದರೆ, ಕೆಳಗಿನ ಬಟನ್ ಮೇಲೆ ಕ್ಲಿಕ್ ಮಾಡಿ.';
+$lang['pptpd_dns_server'] = 'DNS ಸರ್ವರ್';
+$lang['pptpd_dns_server_invalid'] = 'DNS ಸರ್ವರ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['pptpd_ip_range_invalid'] = 'IP ಶ್ರೇಣಿ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['pptpd_local_ip_range'] = 'ಸ್ಥಳೀಯ IP ರೇಂಜ್';
+$lang['pptpd_pptp_server'] = 'PPTP ಸರ್ವರ್';
+$lang['pptpd_remote_ip_range'] = 'ರಿಮೋಟ್ ಐಪಿ ರೇಂಜ್';
+$lang['pptpd_wins_server'] = 'WINS ಸರ್ವರ್';
+$lang['pptpd_wins_server_invalid'] = 'WINS ಸರ್ವರ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
