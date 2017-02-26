f7a9153bd5bf8029c74bbfd79289d623cf9993fb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:26:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN taea ngā kaiwhakamahi ki te hono ki o koutou whatunga mā te whakamahi i te kiritaki VPN noa ki te nuinga o ngā pūnaha whakahaere. He ngāwari, me te whai hua mō te toa rori PPTP engari te whakaaro iti haumaru i te tahi atu hangarau rite OpenVPN.';
+$lang['pptpd_app_name'] = 'Tūmau PPTP';
+$lang['pptpd_auto_configure_help'] = 'Ko tēnei wā te tūmau PPTP i roto i te aratau whirihoranga-aunoa. Ki te huri koe i ō tautuhinga whatunga, ka mohio meinga te tūmau PPTP te huringa tika ki ona tautuhinga. Ki te hiahia koe ki te mono whirihoranga-aunoa, pāwhiria te pātene e whai ake nei.';
+$lang['pptpd_dns_server'] = 'Tūmau DNS';
+$lang['pptpd_dns_server_invalid'] = 'He muhu tūmau DNS.';
+$lang['pptpd_ip_range_invalid'] = 'He muhu awhe IP.';
+$lang['pptpd_local_ip_range'] = 'Awhe IP Local';
+$lang['pptpd_pptp_server'] = 'Tūmau PPTP';
+$lang['pptpd_remote_ip_range'] = 'Awhe IP Mamao';
+$lang['pptpd_wins_server'] = 'Tūmau WINS';
+$lang['pptpd_wins_server_invalid'] = 'He muhu WINS tūmau.';
