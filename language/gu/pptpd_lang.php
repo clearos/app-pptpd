4bb20bd2882e561f976ba16856ca51b5ac9acc0c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:23:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP વીપીએન વપરાશકર્તાઓ મોટા ભાગની ઓપરેટીંગ સિસ્ટમો માટે સામાન્ય એક વીપીએન ક્લાઈન્ટ ઉપયોગ કરીને તમારા નેટવર્ક સાથે જોડાવા માટે પરવાનગી આપે છે. PPTP સરળ અને રોડ વોરિયર્સ માટે ઉપયોગી છે, પરંતુ OpenVPN જેવા અન્ય ટેકનોલોજી કરતાં ઓછો સુરક્ષિત ગણવામાં આવે છે.';
+$lang['pptpd_app_name'] = 'PPTP સર્વર';
+$lang['pptpd_auto_configure_help'] = 'PPTP સર્વર સ્વતઃ રુપરેખાંકનની સ્થિતિ હાલમાં છે. તમે તમારા નેટવર્ક સેટિંગ્સ બદલો તો, PPTP સર્વર intelligently તેના સુયોજનો જરૂરી ફેરફારો કરશે. તમે ઓટો રૂપરેખાંકન નિષ્ક્રિય કરવા માંગો છો, તો નીચેના બટન પર ક્લિક કરો.';
+$lang['pptpd_dns_server'] = 'DNS સર્વર';
+$lang['pptpd_dns_server_invalid'] = 'DNS સર્વર અમાન્ય છે.';
+$lang['pptpd_ip_range_invalid'] = 'IP વિસ્તાર અમાન્ય છે.';
+$lang['pptpd_local_ip_range'] = 'સ્થાનિક IP રેન્જ';
+$lang['pptpd_pptp_server'] = 'PPTP સર્વર';
+$lang['pptpd_remote_ip_range'] = 'દૂરસ્થ IP રેન્જ';
+$lang['pptpd_wins_server'] = 'WINS સર્વર';
+$lang['pptpd_wins_server_invalid'] = 'WINS સર્વર અમાન્ય છે.';
