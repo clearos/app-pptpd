b15ed736e59f59414e02388c6bc0b13dcd06e755
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:25:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN пайдаланушылар операциялық жүйелердің көпшілігі ортақ бір VPN клиентін пайдаланып желіге қосуға мүмкіндік береді. PPTP жол жауынгерлердің оңай және пайдалы болып табылады, бірақ OpenVPN сияқты басқа технологиялар кем қауіпсіз болып саналады.';
+$lang['pptpd_app_name'] = 'PPTP Сервер';
+$lang['pptpd_auto_configure_help'] = 'PPTP сервер авто-конфигурация режимінде қазіргі уақытта. Егер желі параметрлерін өзгертсеңіз, PPTP сервер парасатты оның параметрлеріне қажетті өзгерістер енгізуге болады. Егер сіз автоматты түрде конфигурациясын ажырату келсе, төмендегі батырмасын басыңыз.';
+$lang['pptpd_dns_server'] = 'DNS-сервер';
+$lang['pptpd_dns_server_invalid'] = 'DNS сервері жарамсыз болып табылады.';
+$lang['pptpd_ip_range_invalid'] = 'IP ауқымы жарамсыз.';
+$lang['pptpd_local_ip_range'] = 'Жергілікті IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP Сервер';
+$lang['pptpd_remote_ip_range'] = 'Қашықтан IP Range';
+$lang['pptpd_wins_server'] = 'WINS сервер';
+$lang['pptpd_wins_server_invalid'] = 'WINS сервердің жарамсыз болып табылады.';
