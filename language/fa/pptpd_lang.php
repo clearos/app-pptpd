b95a99af8e1fb3ebe3fb721e907e93e124e5c9d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:28:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN اجازه می دهد تا به کاربران برای اتصال به شبکه خود را با استفاده از یک سرویس گیرنده VPN مشترک به بسیاری از سیستم عامل است. PPTP آسان و مفید برای رزمندگان جاده است، اما نظر گرفته شده است امنیت کمتری نسبت به سایر فن آوری ها مانند OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP سرور';
+$lang['pptpd_auto_configure_help'] = 'سرور PPTP حال حاضر در حالت خودکار پیکربندی. اگر شما تنظیمات شبکه خود را تغییر دهید، سرور PPTP هوشمندانه تغییرات لازم به تنظیمات آن است. اگر شما می خواهم برای غیر فعال کردن خودکار پیکربندی، لطفا بر روی دکمه زیر کلیک کنید.';
+$lang['pptpd_dns_server'] = 'دی ان اس سرور';
+$lang['pptpd_dns_server_invalid'] = 'سرور DNS نامعتبر است.';
+$lang['pptpd_ip_range_invalid'] = 'محدوده IP نامعتبر است.';
+$lang['pptpd_local_ip_range'] = 'محلی IP محدوده';
+$lang['pptpd_pptp_server'] = 'PPTP سرور';
+$lang['pptpd_remote_ip_range'] = 'از راه دور IP محدوده';
+$lang['pptpd_wins_server'] = 'سرور WINS را';
+$lang['pptpd_wins_server_invalid'] = 'سرور WINS را نامعتبر است.';
