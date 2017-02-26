9907fe43c32b9cf8301dfac7f44107f1e482c8c1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:29:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN saamaxaya kuwa isticmaala in ay ku xidhmaan network adiga oo isticmaalaya macmiilka VPN a caadi ah in nidaamka qalliinka inta badan. PPTP waa wax fudud oo waxtar u leh dagaalyahan wadada laakiin waxaa loo arkaa wax ka yar ammaan badan farsamooyinka kale sida OpenVPN.';
+$lang['pptpd_app_name'] = 'Server PPTP';
+$lang['pptpd_auto_configure_help'] = 'server PPTP ayaa haatan in hab auto-qaabeynta. Haddii aad bedesho goobaha network aad, server PPTP ah miyir ka dhigi doonaa isbedel loo baahan yahay in goobaha ay. Haddii aad jeclaan lahayd inaad gab auto-qaabeynta, fadlan riix badhanka soo socda.';
+$lang['pptpd_dns_server'] = 'Server DNS';
+$lang['pptpd_dns_server_invalid'] = 'server DNS waa baadil.';
+$lang['pptpd_ip_range_invalid'] = 'kala duwan IP waa baadil.';
+$lang['pptpd_local_ip_range'] = 'Local IP Range';
+$lang['pptpd_pptp_server'] = 'Server PPTP';
+$lang['pptpd_remote_ip_range'] = 'Remote IP Range';
+$lang['pptpd_wins_server'] = 'WINS Server';
+$lang['pptpd_wins_server_invalid'] = 'server WINS waa baadil.';
