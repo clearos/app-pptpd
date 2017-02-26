cbe8053781afdb785136cf1f286b1de9ab99295d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:22:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN zimathandiza owerenga kugwirizana kwa maukonde anu pogwiritsa ntchito VPN kasitomala wamba kachitidwe kwambiri opaleshoni. PPTP n&#39;zosavuta ndipo lipindulitsa pa asilikali msewu koma imatengedwa otetezeka zosakwana umisiri ena monga OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'The PPTP makina panopa mu mode galimoto-kasinthidwe. Ngati inu sinthani maukonde anu, PPTP makina adzakhala mwanzeru kusintha kwa zoikamo ake. Ngati mukufuna kuletsa galimoto-kasinthidwe, chonde alemba pa batani zotsatirazi.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS Seva ndi opanda pake.';
+$lang['pptpd_ip_range_invalid'] = 'IP osiyanasiyana ndi opanda pake.';
+$lang['pptpd_local_ip_range'] = 'Local IP osiyanasiyana';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Akumidzi IP osiyanasiyana';
+$lang['pptpd_wins_server'] = 'Umapeza Server';
+$lang['pptpd_wins_server_invalid'] = 'Umapeza Seva ndi opanda pake.';
