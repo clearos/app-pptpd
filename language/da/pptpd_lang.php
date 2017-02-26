fdd56b14141c372da0dadd8d16b8bfba5f34f60a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:22:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN giver brugerne mulighed for at oprette forbindelse til dit netværk ved hjælp af en VPN-klient fælles for de fleste operativsystemer. PPTP er nemt og nyttigt for road warriors, men betragtes som mindre sikker end andre teknologier som OpenVPN.';
+$lang['pptpd_app_name'] = 'PPTP-server';
+$lang['pptpd_auto_configure_help'] = 'Den PPTP server er i øjeblikket i auto-konfiguration mode. Hvis du ændrer netværksindstillingerne, vil PPTP server intelligent foretage de nødvendige ændringer i sine indstillinger. Hvis du ønsker at deaktivere automatisk konfiguration, skal du klikke på følgende knap.';
+$lang['pptpd_dns_server'] = 'DNS-server';
+$lang['pptpd_dns_server_invalid'] = 'DNS-server er ugyldig.';
+$lang['pptpd_ip_range_invalid'] = 'IP-område er ugyldigt.';
+$lang['pptpd_local_ip_range'] = 'Local IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP-server';
+$lang['pptpd_remote_ip_range'] = 'Fjernbetjening IP Range';
+$lang['pptpd_wins_server'] = 'WINS-server';
+$lang['pptpd_wins_server_invalid'] = 'WINS-server er ugyldig.';
