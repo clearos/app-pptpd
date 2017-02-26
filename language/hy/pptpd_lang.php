15746f1a372d96809185297dc4de366595a78f30
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:21:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN թույլ է տալիս օգտվողներին միացնել ձեր ցանցը, օգտագործելով VPN հաճախորդին ընդհանուր մասի օպերացիոն համակարգերի. PPTP շատ հեշտ է եւ օգտակար ճանապարհային մարտիկների, բայց համարվում է պակաս անվտանգ է, քան այլ տեխնոլոգիաների, ինչպիսիք OpenVPN:';
+$lang['pptpd_app_name'] = 'PPTP Server';
+$lang['pptpd_auto_configure_help'] = 'The PPTP սերվերը ներկայումս գտնվում է ավտո-կազմաձեւման ռեժիմում. Եթե ​​դուք փոխեք ձեր ցանցային կարգավորումները, որ PPTP սերվերը չի intelligently կատարել անհրաժեշտ փոփոխություններ կատարելու իր պարամետրերը: Եթե ​​ցանկանում եք անջատել ավտոմատ կոնֆիգուրացիան, խնդրում ենք սեղմել հետեւյալ կոճակը.';
+$lang['pptpd_dns_server'] = 'DNS Server';
+$lang['pptpd_dns_server_invalid'] = 'DNS սերվերը անվավեր:';
+$lang['pptpd_ip_range_invalid'] = 'IP միջակայք անվավեր է:';
+$lang['pptpd_local_ip_range'] = 'Տեղական IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP Server';
+$lang['pptpd_remote_ip_range'] = 'Remote IP Range';
+$lang['pptpd_wins_server'] = 'WINS Server';
+$lang['pptpd_wins_server_invalid'] = 'WINS սերվերը անվավեր:';
