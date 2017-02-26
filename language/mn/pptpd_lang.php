f8faacc10d6a9b668b187b245bdb900f5819a18a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:27:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/pptpd_lang.php
@@ -0,0 +1,13 @@
+<?php
+
+$lang['pptpd_app_description'] = 'PPTP VPN хэрэглэгч нийтлэг ихэнх үйлдлийн систем нь VPN үйлчлүүлэгчид ашиглан таны сүлжээнд холбогдох боломжийг олгодог. PPTP зам дайчдын хялбар, ашигтай боловч OpenVPN гэх мэт өөр бусад технологиуд аас аюулгүй гэж үздэг.';
+$lang['pptpd_app_name'] = 'PPTP сервер';
+$lang['pptpd_auto_configure_help'] = 'PPTP сервер автоматаар тохиргоо горимд одоогоор байна. Хэрэв та сүлжээний тохиргоог өөрчлөх бол PPTP сервер ухаалгаар түүний тохиргоо шаардлагатай өөрчлөлтүүдийг хийж болно. Хэрэв та автомат тохиргоог идэвхгүй болгохыг хүсэж байгаа бол дараах товчин дээр дарна уу.';
+$lang['pptpd_dns_server'] = 'DNS сервер';
+$lang['pptpd_dns_server_invalid'] = 'DNS сервер хүчин төгөлдөр бус байна.';
+$lang['pptpd_ip_range_invalid'] = 'IP хүрээ хүчин төгөлдөр бус байна.';
+$lang['pptpd_local_ip_range'] = 'Орон нутгийн IP Range';
+$lang['pptpd_pptp_server'] = 'PPTP сервер';
+$lang['pptpd_remote_ip_range'] = 'Алсын IP Range';
+$lang['pptpd_wins_server'] = 'хожиж Server';
+$lang['pptpd_wins_server_invalid'] = 'Хожиж серверийн хүчин төгөлдөр бус байна.';
